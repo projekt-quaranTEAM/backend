7b7f3d7e42f58e3c1baa534f33d0c48d99171c80
Piotrek <amrozp@gmail.com>
2021-01-12 19:50:02 +0100

    [API] Fix Survey controller

--- src/main/java/pl/programowaniezespolowe/planner/controllers/SurveyController.java
+++ src/main/java/pl/programowaniezespolowe/planner/controllers/SurveyController.java
@@ -3,6 +3,7 @@ package pl.programowaniezespolowe.planner.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,8 @@ import java.util.Map;
 
 @RestController
 public class SurveyController {
+
+    int userID = 3;
     @Autowired
     ActivityRepository activityRepository;
 
@@ -26,27 +29,29 @@ public class SurveyController {
     List<String> li = Arrays.asList("Sport" ,"Meeting","Movies","Cooking","Music","Games","Books","Art","Technology",
                                     "Housework","Community","Business", "Health","Science","Travel","Charity",
                                     "Spirituality","Family","Education","Holiday","Fashion", "Auto");
-
+    @CrossOrigin
     @PostMapping("/register/survey")
     public ResponseEntity<?> survey(@RequestBody Map<String, String> body) {
-        Activity activity1 = new Activity();
-        for(String activity : li) {
-            if(body.get(activity) != null) {
-               activity1.setName(activity);
-               activity1.setAmount(Integer.parseInt(body.get(activity)));
-
-                List<User> users = userRepository.findAll();
-                for(User u : users) {
-                    if(u.getEmail().equals(body.get("email"))) {
-                        activity1.setUserid(u.getId());
-                    }
-                }
 
 
+        List<User> users = userRepository.findAll();
+
+        for(User u : users) {
+            if(u.getEmail().equals(body.get("email"))) {
+                this.userID = u.getId();
             }
-            activityRepository.save(activity1);
         }
 
+        for(String activity : li) {
+            Activity activity1 = new Activity();
+            if(body.get(activity.toLowerCase()) != null) {
+                activity1.setName(activity);
+                activity1.setAmount(Integer.parseInt(body.get(activity.toLowerCase())));
+                activity1.setUserid(this.userID);
+                activityRepository.save(activity1);
+            }
+        }
+        
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
